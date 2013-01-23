a0275275e224f4b3f02d1931da178cc88d0020ae
Devon Bryant <devon.bryant@issinc.com>
2013-01-22 19:16:52 -0700

    Updated FFT to use pure functional algorithm

--- safe-core/src/main/scala/safe/math/FFT.scala
+++ safe-core/src/main/scala/safe/math/FFT.scala
@@ -1,32 +1,35 @@
 package safe.math
 
 object FFT {
-  import scala.collection.mutable
   import scala.math._
   
   def fft(data: Seq[Complex]): Seq[Complex] = {
     require((data.length & data.length - 1) == 0, 
         "Cannot calculate fft for length " + data.length + ", must be a power of 2")
         
-    ditfft(data)
+    ditfft2(data)
   }
   
   // Cooley-Turkey FFT
-  private def ditfft(data: Seq[Complex]): Seq[Complex] = {
+  private[this] def ditfft2(data: Seq[Complex]): Seq[Complex] = {
     data.length match {
       case 0 => Nil
       case 1 => data
       case n => {
-        val evens = ditfft(data.zipWithIndex.filter(_._2 % 2 == 0).map(_._1))
-        val odds = ditfft(data.zipWithIndex.filter(_._2 % 2 != 0).map(_._1))
-        val buf = Array.ofDim[Complex](n)
-        for (i <- 0 to n/2 - 1) {
+        val evens = ditfft2(data.zipWithIndex filter { _._2 % 2 == 0 } map { _._1 })
+        val odds = ditfft2(data.zipWithIndex filter { _._2 % 2 != 0 } map { _._1 })
+        val phis = for (i <- 0 to n/2 - 1) yield {
           val phi = -2.0 * Pi * i/n
-          val phic = Complex(cos(phi), sin(phi))
-          buf(i) = evens(i) + odds(i) * phic
-          buf(i + n/2) = evens(i) - odds(i) * phic
+          Complex(cos(phi), sin(phi))
         }
-        buf.toSeq
+        
+        // one = evens(i) + odds(i) * phis(i)
+        // two = evens(i) - odds(i) * phis(i)
+        val ops = (odds, phis).zipped map { _ * _ }
+        val one = (evens, ops).zipped map { _ + _ }
+        val two = (evens, ops).zipped map { _ - _ }
+        
+        one ++ two
       }
     }
   }
