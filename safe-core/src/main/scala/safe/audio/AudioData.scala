4d595bd6e37249743018ec46308f336a54479f3a
Devon Bryant <devon.bryant@issinc.com>
2013-02-01 17:02:42 -0700

    Minor update to channel merge function

--- safe-core/src/main/scala/safe/audio/AudioData.scala
+++ safe-core/src/main/scala/safe/audio/AudioData.scala
@@ -38,11 +38,12 @@ case class AudioAggregate(id: String,
 object AudioFunctions {
   /** Merge a set of channels into a single sequence by averaging indexed elements */
   def merge[A](channels: Channels[Seq[A]])(implicit num: Integral[A]) = {
+    val div = num.fromInt(channels.size)
     channels.foldLeft(Seq[A]()) {
       case (Nil, Nil) => Nil
       case (a, Nil) => a
       case (Nil, b) => b
       case (a, b) => (a, b).zipped map { num.plus(_, _) }
-    } map { num.quot(_, num.fromInt(channels.size)) }
+    } map { num.quot(_, div) }
   }
 }
