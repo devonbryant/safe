10333a7ec8f456f993dc8683a296325d2bbff394
Devon Bryant <devonbryant@gmail.com>
2013-05-18 12:32:45 -0600

    Added stream for MIDI note frequencies

--- safe-core/src/main/scala/safe/audio/MIDI.scala
+++ safe-core/src/main/scala/safe/audio/MIDI.scala
@@ -4,4 +4,9 @@ package safe.audio
 object MIDI {
   /** Get the frequency (hz) of a given MIDI note number */
   def noteFreq(n: Int) = (440.0 / 32) * math.pow(2, (n - 9).toDouble / 12)
+  
+  def noteFreqs(): Stream[Double] = {
+    def recur(n: Int): Stream[Double] = noteFreq(n) #:: recur(n + 1)
+    recur(0)
+  }
 }
