64a81193ae19af03014e9d4dad3ee3142689d6b0
Devon Bryant <devonbryant@gmail.com>
2013-05-18 13:00:34 -0600

    Updated tests for vector add/sub

--- safe-core/src/test/scala/safe/SafeVectorSpec.scala
+++ safe-core/src/test/scala/safe/SafeVectorSpec.scala
@@ -81,35 +81,53 @@ class SafeVectorSpec extends SafeVectorMatchers
     ab should equal (SafeVector(2.0, 6.0))
   }
   
+  it should "allow addition with other vectors" in {
+    val a_plus_b = SafeVector(1,2) + SafeVector(2,3)
+    
+    a_plus_b should equal (SafeVector(3,5))
+  }
+  
+  it should "allow subtraction with other vectors" in {
+    val a_minus_b = SafeVector(2,4) - SafeVector(2,3)
+    
+    a_minus_b should equal (SafeVector(0,1))
+  }
+  
   val lengths = for { n <- Gen.choose(0, 100) } yield n
   val factors = for { fac <- Gen.choose(-100.0, 100.0) } yield fac
   
-  // Hadamard mult should be commutative
-  it should "have commutative hadamard products" in {
+  // Addition and Hadamard mult should be commutative
+  it should "have commutative addition and hadamard products" in {
     forAll (lengths, factors) { (n: Int, fac: Double) =>
       whenever (n >= 0) {
         val a = SafeVector.fill(n) { scala.util.Random.nextDouble * fac }
         val b = SafeVector.fill(n) { scala.util.Random.nextDouble * fac }
     
+        val a_plus_b = a + b
+        val b_plus_a = b + a
         val ab = a :* b
         val ba = b :* a
     
+        a_plus_b should equalWithTolerance (b_plus_a, 1e-6)
         ab should equalWithTolerance (ab, 1e-6)
       }
     }
   }
   
-  // Hadamard mult should be associative
-  it should "have associative hadamard products" in {
+  // Addition and Hadamard mult should be associative
+  it should "have associative addition and hadamard products" in {
     forAll (lengths, factors) { (n: Int, fac: Double) =>
       whenever (n >= 0) {
         val a = SafeVector.fill(n) { scala.util.Random.nextDouble * fac }
         val b = SafeVector.fill(n) { scala.util.Random.nextDouble * fac }
         val c = SafeVector.fill(n) { scala.util.Random.nextDouble * fac }
     
+        val aplusb_plusc = (a + b) + c
+        val aplus_bplusc = a + (b + c)
         val ab_c = (a :* b) :* c
         val a_bc = a :* (b :* c)
     
+        aplusb_plusc should equalWithTolerance (aplus_bplusc, 1e-6)
         ab_c should equalWithTolerance (a_bc, 1e-6)
       }
     }
